023c12a6feb3141e470089a82669d67777fe89b4
Robert Saylor <robert@customphpdesign.com>
2016-03-22 13:05:00 -0400

    development

--- class/contacts.class.php
+++ class/contacts.class.php
@@ -3,10 +3,10 @@ include $GLOBAL['path']."/class/reservations.class.php";
 
 class contacts extends reservations {
 
-		public function contacts() {
+	public function contacts() {
 			$template = "contacts.tpl";
-			$data['country'] = $this->country_list($null);
 			$data['list'] = $this->list_contacts();
+			$data['country'] = $this->country_list($null);
     		$this->load_smarty($data,$template);
 		}
 
