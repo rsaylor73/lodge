0c8f48ec8fee197e101640086b9775458d381028
Robert Saylor <robert@customphpdesign.com>
2016-03-28 14:47:58 -0400

    development

--- ajax/lookup_reseller.php
+++ ajax/lookup_reseller.php
@@ -32,16 +32,34 @@ if ($check == "FALSE") {
 		ORDER BY `a`.`first` ASC, `a`.`last` ASC
 		";
 	} elseif ($_GET['company'] != "") {
-		# code...
-	} else {
+		$sql = "
+		SELECT
+			`r`.`company`,
+			`r`.`resellerID`,
+			`a`.`reseller_agentID`,
+			`a`.`first`,
+			`a`.`last`
 
+		FROM
+			`reserve`.`resellers` r, `reserve`.`reseller_agents` a
+
+		WHERE
+			`r`.`company` LIKE '%$_GET[company]%'
+			AND `r`.`resellerID` = `a`.`resellerID`
+			AND `a`.`status` = 'Active'
+
+		ORDER BY `a`.`first` ASC, `a`.`last` ASC
+		";
+	} else {
+		print "<br><font color=red>Please enter in a company name or a reseller ID.</font><br>";
+		die;
 	}
 
 	$result = $core->new_mysql($sql);
 	while ($row = $result->fetch_assoc()) {
 		if ($this_company != $row['company']) {
 			$html .= "<tr><td colspan=2><b>$row[company]</b></td></tr>";
-			$company = $row['company'];
+			$this_company = $row['company'];
 		}
 		$html .= "<tr><td width=\"150\">$row[first] $row[last]</td><td><input type=\"button\" onclick=\"document.location.href='assignagenttoreservation/$_GET[reservationID]/$row[reseller_agentID]'\" class=\"btn btn-primary\" value=\"Assign Agent\"></td></tr>";
 	}
