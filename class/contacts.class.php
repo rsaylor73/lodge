00bd7addac06b50054a8895efaded48bc5ea428f
Robert Saylor <robert@customphpdesign.com>
2016-03-31 14:48:54 -0400

    development

--- class/contacts.class.php
+++ class/contacts.class.php
@@ -125,7 +125,7 @@ class contacts extends reservations {
 		$sql = "UPDATE `reserve`.`contacts` c SET
 
 		`title` = '$_POST[title]', `first` = '$_POST[first]', `middle` = '$_POST[middle]', `last` = '$_POST[last]', `email` = '$_POST[email]', `address1` = '$_POST[address1]', `address2` = '$_POST[address2]',
-		`city` = '$_POST[city]', `state` = '$_POST[state]', `province` = '$_POST[province]', `countryID` = '$_POST[country]', `zip` = '$_POST[zip]', `date_of_birth` = '$_POST[dob]',
+		`city` = '$_POST[city]', `state` = '$_POST[state]', `province` = '$_POST[province]', `countryID` = '$_POST[country]', `zip` = '$_POST[zip]', `date_of_birth` = DATE_FORMAT('$_POST[dob]','%m/%d/%Y'),
 		`phone1_type` = '$_POST[phone1_type]', `phone2_type` = '$_POST[phone2_type]', `phone3_type` = '$_POST[phone3_type]', `phone4_type` = '$_POST[phone4_type]',
 		`phone1` = '$_POST[phone1]', `phone2` = '$_POST[phone2]', `phone3` = '$_POST[phone3]', `phone4` = '$_POST[phone4]'
 
