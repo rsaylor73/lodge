b1fe72a6d2c3cab8c9034e377118d381e541b430
Robert Saylor <robert@customphpdesign.com>
2016-04-04 10:38:11 -0400

    development

--- class/reservations.class.php
+++ class/reservations.class.php
@@ -345,6 +345,8 @@ class reservations extends money {
 		HAVING total_adult_beds >= '$adults' AND total_child_beds >= '$children'
 		";
 
+		print "SQL:<br>$sql<br>";
+
 		$data['nights'] = $_POST['nights'];
 		$data['adults'] = $_POST['pax'];
 		$data['children'] = $_POST['children'];
