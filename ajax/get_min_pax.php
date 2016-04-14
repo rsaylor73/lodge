57a2f6c71fe79de41a54d1238e14942633f49362
Robert Saylor <robert@customphpdesign.com>
2016-04-14 08:22:27 -0400

    development

--- ajax/get_min_pax.php
+++ ajax/get_min_pax.php
@@ -97,7 +97,7 @@ if ($check == "FALSE") {
 
 	if ($adults != "") {
 		$adult1 = $adult - $adults;
-		print "<br><font color=blue><b>Max $adult1 Adults OR $adult Adults and $child Children</b></font>";
+		print "<br><font color=blue>Max $adult1 Adults OR $adults Adults and $child Children</font>";
 	}
 }
 ?>
