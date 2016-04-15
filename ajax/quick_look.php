7997dadd21f799888b4d0e46e2f628bae9bacb41
Robert Saylor <robert@customphpdesign.com>
2016-04-15 13:41:49 -0400

    development

--- ajax/quick_look.php
+++ ajax/quick_look.php
@@ -13,12 +13,63 @@ if ($check == "FALSE") {
 } else {
 
 
-$rand = rand(200,2000);
-print "Test $rand<br>";
-print "<pre>";
-print_r($_GET);
-print_r($_POST);
-print "</pre>";
+		$adults = $_GET['pax'] * $nights;
+		if ($_GET['children'] > 0) {
+			$children = $_GET['children'] * $nights;
+		} else {
+			$children = "0";
+		}
+
+		if ($_GET['tents'] > 1) {
+			$adults = $adults / $_GET['tents'];
+			$children = $children / $_GET['tents'];
+
+		}
+
+		
+		if ($_GET['children'] == "0") {
+			//
+			$child_sql = "AND total_child_beds = '0'";
+		} else {
+			$child_sql = "AND total_child_beds >= '$children'";
+		}
+
+		if ($_GET['type'] != "") {
+			$type = "AND `r`.`type` = '$_GET[type]'";
+		}
+
+		$sql = "
+		SELECT
+			`r`.`id`,
+			`r`.`description`,
+			COUNT(`a`.`status`) AS 'total_adult_beds',
+			COUNT(`c`.`status`) AS 'total_child_beds',
+			`r`.`nightly_rate`,
+			`r`.`beds` AS 'adult',
+			`r`.`children`,
+			`a`.`status` AS 'adult_status',
+			`c`.`status` AS 'child_status'
+
+		FROM
+			`inventory` i, `rooms` r
+
+		LEFT JOIN `beds` a ON `i`.`inventoryID` = `a`.`inventoryID` AND `a`.`type` = 'adult' AND `a`.`status` = 'avail'
+		LEFT JOIN `beds` c ON `i`.`inventoryID` = `c`.`inventoryID` AND `c`.`type` = 'child' AND `c`.`status` = 'avail'
+
+		WHERE
+			`i`.`locationID` = '$_POST[lodge]' 
+			AND `i`.`date_code` BETWEEN '$start_date' AND '$end_date'
+			AND `i`.`roomID` = `r`.`id`
+			$type
+
+		GROUP BY `r`.`description`
+
+		HAVING total_adult_beds >= '$adults' $child_sql
+		";
+
+		print "<pre>";
+		print "$sql";
+		print "</pre>";
 
 
 }
