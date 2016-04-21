a70a031f99bf601285ca1cb4446cd2493a429d9e
Robert Saylor <robert@customphpdesign.com>
2016-04-21 09:34:32 -0400

    development

--- class/reservations.class.php
+++ class/reservations.class.php
@@ -126,6 +126,10 @@ class reservations extends money {
 			$data['min_night_stay'] = $row['min_night_stay'];
 		}
 
+		if ($_POST['type'] != "") {
+			$type = "AND `r`.`type` = '$_POST[type]'";
+		}
+
 		$sql = "
 		SELECT 
 			COUNT(`a`.`status`) AS 'total_adult_beds',
@@ -142,6 +146,7 @@ class reservations extends money {
 			`i`.`locationID` = '$_POST[lodge]' 
 			AND `i`.`date_code` BETWEEN '$start_date' AND '$end_date' 
 			AND `i`.`roomID` = `r`.`id`
+			$type
 
 		HAVING 
 			total_adult_beds >= '$_POST[pax]' AND total_child_beds >= '$_POST[children]'
