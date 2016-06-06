8de91554762f9be3155875db28fb8ef68d994998
Robert Saylor <robert@customphpdesign.com>
2016-06-06 11:04:15 -0400

    development

--- class/reservations.class.php
+++ class/reservations.class.php
@@ -1599,8 +1599,7 @@ class reservations extends money {
 
 	public function savenewnote() {
 		$date = date("Ymd");
-		$sql = "INSERT INTO `notes` (`reservationID`,`userID`,`note`,`timestamp`,`type`) VALUES ('$_POST[reservationID]','$_SESSION[id]','$_POST[note]','$date',$_POST[type]')";
-		print "$sql<br>";
+		$sql = "INSERT INTO `notes` (`reservationID`,`userID`,`note`,`timestamp`,`type`) VALUES ('$_POST[reservationID]','$_SESSION[id]','$_POST[note]','$date','$_POST[type]')";
 		$result = $this->new_mysql($sql);
 
 		if ($result == "TRUE") {
